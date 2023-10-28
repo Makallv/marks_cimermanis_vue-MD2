b773ef5b84dcb90c0ab4ba5f0aee2684fd1d42a3
Makallv <marks.cimermanis@gmail.com>
2023-10-28 16:19:42 +0300

    First setup for MD2

---
+++ src/components/NavigationComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <router-link to="/home" :class="{ active: isRouteActive }">Home</router-link>
+    <router-link to="/about-me" :class="{ active: isRouteActive }">About Me</router-link>
+  </div>
+</template>
+
+<style>
+.active {
+  color: blue;
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    isRouteActive(route) {
+      return this.$route === route;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  color: #f2f2f2;
+  background-color: #8645E8;
+  text-decoration: none;
+}
+</style>
